18f714a1226d2656f7de95f5918da0b1d0776fd8
Everson Silva <everson.csilva99@gmail.com>
2021-10-20 15:12:10 -0300

    run py3

--- main.py
+++ main.py
@@ -1,32 +1,33 @@
 import datetime
-from discord.ext import commands
+
 import discord
 import asyncio
 import os
 from dotenv import load_dotenv
 import pyexcel_ods
-import json
 
 load_dotenv()
-TOKEN       = os.getenv('TOKEN')
-CHANNEL_ID  = os.getenv('CHANNEL_ID')
-client      = discord.Client()
-channel     = None
+TOKEN = os.getenv('TOKEN')
+CHANNEL_ID = os.getenv('CHANNEL_ID')
+client = discord.Client()
+channel = None
 semana = {0: 'segunda', 1: 'terca', 2: 'quarta', 3: 'quinta', 4: 'sexta', 5: 'sabado', 6: 'domingo'}
+allowed_mentions = discord.AllowedMentions(everyone=True)
+
+
 def excel(diaDaSemana='domingo'):
-    data = pyexcel_ods.get_data("emissarioList/"+diaDaSemana+".ods")
+    data = pyexcel_ods.get_data("emissarioList/" + diaDaSemana + ".ods")
     # print()
     excel = None
     for d in data.items():
         for f in d:
             excel = f
-    #print(excel)
+    # print(excel)
     del excel[0:2]
     del excel[-1]
     del excel[-1]
     return excel
 
-allowed_mentions = discord.AllowedMentions(everyone = True)
 async def my_background_task():
     await client.wait_until_ready()
     # channel = client.get_channel(id=894572084670373908) # replace with channel_id
@@ -35,7 +36,7 @@ async def my_background_task():
         # Para cada servidor
         for server in servers:
             for channel in server.channels:
-                #importar
+                # importar
                 for e in excel(semana[datetime.datetime.today().weekday()]):
                     horaDoBoss = str(format(e[2], '%H:%M'))
                     # now = datetime.datetime.now() + datetime.timedelta(minutes=19) #para testes
@@ -44,16 +45,18 @@ async def my_background_task():
                     # print(now)
                     # print(horaDoBoss)
                     # print(now==horaDoBoss)
-                    if(horaDoBoss == now):
-                        mensagem= "Atenção!\n@everyone\n"+e[0]+"de Lv. "+str(e[1])+" Apareceu! \nLocalização: "+e[3]+"\nRecompensa: "+e[4]+"\nFama points: "+str(e[5])+"\nObservações adicionais: "+e[6]
+                    if (horaDoBoss == now):
+                        mensagem = "Atenção!\n@everyone\n" + e[0] + "de Lv. " + str(e[1]) + " Apareceu! \nLocalização: " + e[3] + "\nRecompensa: " + e[4] + "\nFama points: " + str(e[5]) + "\nObservações adicionais: " + e[6]
                         await channel.send(content=mensagem, allowed_mentions=allowed_mentions)
-        await asyncio.sleep(60) # task runs every 60 seconds
+        await asyncio.sleep(60)  # task runs every 60 seconds
+
 
 def montarMensagem(diaDaSemana):
     mensagem = ""
     for e in excel(diaDaSemana):
         mensagem += "\n-----\nHorário: " + str(e[2]) + "\n" + e[0] + "de Lv. " + str(e[1]) + "\nLocalização: " + e[3] + "\nRecompensa: " + e[4] + "\nFama points: " + str(e[5]) + "\nObservações adicionais: " + e[6] + "\n-----"
     return mensagem
+
 
 @client.event
 async def on_message(message):
@@ -62,47 +65,47 @@ async def on_message(message):
 
     prefix = '!'
     if message.content.startswith(prefix):
-        diaDaSemana = ['segunda','terca','quarta','quinta','sexta','sabado','domingo']
+        diaDaSemana = ['segunda', 'terca', 'quarta', 'quinta', 'sexta', 'sabado', 'domingo']
         carregando = 'Carregando lista de emissários...'
         sucesso = 'Carregado! Aqui está: '
 
-        if message.content.startswith(prefix+diaDaSemana[0]):
+        if message.content.startswith(prefix + diaDaSemana[0]):
             await message.channel.send(carregando)
             m = montarMensagem(diaDaSemana[0])
             await message.channel.send(sucesso)
             await message.channel.send(m)
 
-        if message.content.startswith(prefix+diaDaSemana[1]):
+        if message.content.startswith(prefix + diaDaSemana[1]):
             await message.channel.send(carregando)
             m = montarMensagem(diaDaSemana[0])
             await message.channel.send(sucesso)
             await message.channel.send(m)
 
-        if message.content.startswith(prefix+diaDaSemana[2]):
+        if message.content.startswith(prefix + diaDaSemana[2]):
             await message.channel.send(carregando)
             m = montarMensagem(diaDaSemana[2])
             await message.channel.send(sucesso)
             await message.channel.send(m)
 
-        if message.content.startswith(prefix+diaDaSemana[3]):
+        if message.content.startswith(prefix + diaDaSemana[3]):
             await message.channel.send(carregando)
             m = montarMensagem(diaDaSemana[3])
             await message.channel.send(sucesso)
             await message.channel.send(m)
 
-        if message.content.startswith(prefix+diaDaSemana[4]):
+        if message.content.startswith(prefix + diaDaSemana[4]):
             await message.channel.send(carregando)
             m = montarMensagem(diaDaSemana[4])
             await message.channel.send(sucesso)
             await message.channel.send(m)
 
-        if message.content.startswith(prefix+diaDaSemana[5]):
+        if message.content.startswith(prefix + diaDaSemana[5]):
             await message.channel.send(carregando)
             m = montarMensagem(diaDaSemana[5])
             await message.channel.send(sucesso)
             await message.channel.send(m)
 
-        if message.content.startswith(prefix+diaDaSemana[6]):
+        if message.content.startswith(prefix + diaDaSemana[6]):
             await message.channel.send(carregando)
             m = montarMensagem(diaDaSemana[6])
             await message.channel.send(sucesso)
@@ -117,5 +120,6 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
-client.loop.create_task(my_background_task())
-client.run(TOKEN)
+if __name__ == "__main__":
+    client.loop.create_task(my_background_task())
+    client.run(TOKEN)
